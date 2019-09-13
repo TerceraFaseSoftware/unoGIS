b8a41175e9d6c36fef2893cbcd86ff9f0d0d4ca8
DanielVivar <47527746+DanielVivar@users.noreply.github.com>
2019-09-13 11:11:13 +0200

    WFSServices

---
+++ Example2_WFSServices/WFSServices/Controllers/WFSController.cs
@@ -0,0 +1,38 @@
+﻿/*Add the packages that we will use during the code*/
+using System.Web.Mvc;
+using unoGIS.SpatialObjects;
+using unoGIS.Services.OGC.WFS;
+
+namespace WFSServices.Controllers
+{
+    public class WFSController : Controller
+    {
+        /*Declare a ISpatialDriver variable*/
+        static ISpatialDriver driver;
+        static WFSController()
+        {
+            string subPath = @"App_Data\DataExamples\shapefile";
+            string fullPath = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, subPath);
+
+            /*Initialize the variable to connect the type "shapefile" data that we have saved in a folder*/
+            driver = new unoGIS.SpatialDrivers.ShapeFileSpatialDriver.ShapefileFolderSpatialDriver
+            {
+                Connection = new SpatialConnectionBuilder()
+                {
+                    /*In the DataSource we will put the route of the folder with the data*/
+                    DataSource = fullPath
+                }
+            };
+
+            driver.Open();
+        }
+
+        public void Index()
+        {
+            /*To realize the connection of the WFS service, first we will put the specific information of the HTTP,
+             * at second, the name that the service will receive 
+             * and finally, we will introduce the entities of the shapefile */
+            WFSService.Response(this.HttpContext, "Example_Name", driver.EntitySets);
+        }
+    }
+}
---
+++ Example2_WFSServices/WFSServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WFSServices.MvcApplication" Language="C#" %>
